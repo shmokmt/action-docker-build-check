b02fe8f5a20a63b82a23aca8fc9d249eb1a9925b
shmokmt <32533860+shmokmt@users.noreply.github.com>
2024-10-24 07:19:45 +0900

    delete dup vars

--- script.sh
+++ script.sh
@@ -28,8 +28,6 @@ echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/review
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
 docker_build_jq='{
  source: {
   name: "docker-build-check",
